4ff798b8a9daf948e176f73921e9dd523b0c8f49
youchou <lilyoutch@yahoo.fr>
2014-03-21 12:02:23 +0100

    yes

---
+++ Labo1/Labo1/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿using System.Windows.Forms;
+namespace Labo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (MessageBox.Show("Fermer la fenêtre", "Alert", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                if (disposing && (components != null))
+                {
+                    components.Dispose();
+                }
+                base.Dispose(disposing);
+            }
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.panelDessin = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(36, 26);
+            this.buttonClose.Margin = new System.Windows.Forms.Padding(9, 7, 9, 7);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(225, 50);
+            this.buttonClose.TabIndex = 0;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(591, 26);
+            this.buttonOpen.Margin = new System.Windows.Forms.Padding(9, 7, 9, 7);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(225, 50);
+            this.buttonOpen.TabIndex = 1;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // panelDessin
+            // 
+            this.panelDessin.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panelDessin.Location = new System.Drawing.Point(36, 89);
+            this.panelDessin.Margin = new System.Windows.Forms.Padding(9, 7, 9, 7);
+            this.panelDessin.Name = "panelDessin";
+            this.panelDessin.Size = new System.Drawing.Size(780, 453);
+            this.panelDessin.TabIndex = 2;
+            this.panelDessin.Click += new System.EventHandler(this.panelDessin_Click);
+            this.panelDessin.Paint += new System.Windows.Forms.PaintEventHandler(this.panelDessin_Paint);
+            this.panelDessin.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelDessin_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 568);
+            this.Controls.Add(this.panelDessin);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.buttonClose);
+            this.Margin = new System.Windows.Forms.Padding(9, 7, 9, 7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClose;
+        private Button buttonOpen;
+        private Panel panelDessin;
+    }
+}
+
---
+++ Labo1/Labo1/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Labo1
+{
+    public partial class Form1 : Form
+    {
+        private List<Rectangle> points = new List<Rectangle>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Fermer la fenêtre","Alert",MessageBoxButtons.YesNo) == DialogResult.Yes)
+                this.Dispose();
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            Form2 nouvelleFenetre = new Form2();
+         //   nouvelleFenetre.ShowDialog();
+            nouvelleFenetre.Show();
+        }
+
+        private void panelDessin_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs souris = (MouseEventArgs)e;
+            Rectangle contEllipse = new Rectangle();
+            Point position = new Point();
+            Size taille = new Size(10,10);
+            position.X = souris.X - taille.Width/2;
+            position.Y = souris.Y - taille.Height/2;
+            contEllipse.Location = position;
+            contEllipse.Size = taille;
+            points.Add(contEllipse);
+            panelDessin.CreateGraphics().FillEllipse(new SolidBrush(Color.Blue), contEllipse);
+        }
+
+        private void panelDessin_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Rectangle rect in points)
+            {
+                panelDessin.CreateGraphics().FillEllipse(new SolidBrush(Color.Blue), rect);
+            }
+        }
+
+        private void panelDessin_MouseMove(object sender, MouseEventArgs e)
+        {
+            MouseEventArgs souris = (MouseEventArgs)e;
+            if (souris.Button == MouseButtons.Left)
+            {
+                Rectangle contEllipse = new Rectangle();
+                Point position = new Point();
+                Size taille = new Size(10, 10);
+                position.X = souris.X - taille.Width / 2;
+                position.Y = souris.Y - taille.Height / 2;
+                contEllipse.Location = position;
+                contEllipse.Size = taille;
+                points.Add(contEllipse);
+                panelDessin.CreateGraphics().FillEllipse(new SolidBrush(Color.Blue), contEllipse);
+            }
+        }
+    }
+}
